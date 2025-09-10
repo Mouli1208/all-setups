0bbf0d175b7ae4b8aa2224bf570a70a7a2da97a9
Mouli1208 <moulikamanthripragada@gmail.com>
2025-09-10 10:14:20 -0500

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Install Java
+# Step 1: Install Java 17 (Amazon Corretto)
 yum install java-17-amazon-corretto -y
 
-# Download and extract Tomcat
+# Step 2: Download and extract Apache Tomcat 9
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.109/bin/apache-tomcat-9.0.109.tar.gz
 tar -xzf apache-tomcat-9.0.109.tar.gz
+cd apache-tomcat-9.0.109
 
-# Add user and roles
-echo '<role rolename="manager-gui"/>' >> apache-tomcat-9.0.109/conf/tomcat-users.xml
-echo '<role rolename="manager-script"/>' >> apache-tomcat-9.0.109/conf/tomcat-users.xml
-echo '<user username="tomcat" password="admin@123" roles="manager-gui,manager-script"/>' >> apache-tomcat-9.0.109/conf/tomcat-users.xml
+# Step 3: Configure Tomcat users for Manager access
+cat <<EOF > conf/tomcat-users.xml
+<tomcat-users>
+  <role rolename="manager-gui"/>
+  <role rolename="manager-script"/>
+  <user username="tomcat" password="admin@123" roles="manager-gui,manager-script"/>
+</tomcat-users>
+EOF
 
-# Remove access restrictions
-sed -i '/<Valve/d' apache-tomcat-9.0.109/webapps/manager/META-INF/context.xml
+# Step 4: Remove access restrictions from Manager app
+cat <<EOF > webapps/manager/META-INF/context.xml
+<Context privileged="true" antiResourceLocking="false" />
+EOF
 
-# Start Tomcat
-sh apache-tomcat-9.0.109/bin/startup.sh
+# Step 5: Start Tomcat
+sh bin/startup.sh
