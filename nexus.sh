0bb74a35c44afbed348d5d47f283e929954e68ba
Mouli1208 <moulikamanthripragada@gmail.com>
2025-09-10 23:53:40 -0500

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -8,7 +8,7 @@ sudo mv nexus-3.79.1-04 nexus
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/nexus
 sudo chown -R nexus:nexus /app/sonatype*
-sudo sed -i '27  run_as_user="nexus"' /app/nexus/bin/nexus
+sudo sed -i '27 run_as_user="nexus"' /app/nexus/bin/nexus
 sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
 [Unit]
 Description=nexus service
@@ -31,4 +31,3 @@ sudo chkconfig nexus on
 sudo systemctl start nexus
 sudo systemctl enable nexus
 sudo systemctl status nexus
-sudo wget https://download.sonatype.com/nexus/3/nexus-3.79.1-04-linux-x86_64.tar.gz
